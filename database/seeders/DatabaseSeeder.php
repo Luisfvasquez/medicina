811d5bcab72a9ea1fa21201bfd7bdb9faabbab0d
prodigy00 <calitoalejandro184@gmail.com>
2026-07-10 19:53:18 -0400

    feat(database): add medication catalog seeder for local development testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             ClinicScheduleSeeder::class,
             FormTemplateSeeder::class,
             CatalogSeeder::class,
+            MedicationSeeder::class,
             DoctorScheduleSeeder::class,
             DoctorBranchScheduleSeeder::class,
         ]);
